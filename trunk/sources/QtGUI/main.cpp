fc960f1ebc88466eb1d3edca9c02a0e868a19144
borisshr <borisshr@3b23a2e6-e73d-11dd-b2d9-215335d0b316>
2009-02-02 23:14:07 +0000

    Created GUI project. Will be using QT for interface development

---
+++ trunk/sources/QtGUI/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * File: main.cpp - Entry point for GUI part
+ * Copyright (C) 2009  Boris Shurygin
+ */
+#include <QtGui>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    
+    return app.exec();
+}
