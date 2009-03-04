a65228b6879cd62673a56b80ceaad44485a6bdf9
Boris Shurygin <boris-shurygin@users.noreply.github.com>
2009-03-04 10:29:08 +0000

    Playing around with edge edition

--- sources/QtGUI/edge_w.h
+++ sources/QtGUI/edge_w.h
@@ -19,7 +19,6 @@ class EdgeControl: public QObject, public QGraphicsItem
     bool isFixed;
 public: 
         
-    
     enum {Type = TypeEdgeControl};
 
     inline void setFixed( bool fixed = true)
@@ -58,69 +57,15 @@ public:
         return Type;
     }
 
-    EdgeControl( EdgeW* e, QGraphicsScene* scene):
-        QObject(),
-        QGraphicsItem( 0, scene),
-        edge( e),
-        isFixed( false)
-    {
-        setFlag( ItemIsMovable);
-        //setFlag(ItemIsSelectable);
-        setCacheMode(DeviceCoordinateCache);
-        setZValue(1);
-        setPred( NULL);
-        setSucc( NULL);
-    }
-
+    EdgeControl( EdgeW* e, QGraphicsScene* scene);
     ~EdgeControl();
 
-    QRectF boundingRect() const
-    {
-        qreal adjust = 2;
-        return QRectF( -EdgeControlSize - adjust, -EdgeControlSize - adjust,
-                      2*( EdgeControlSize + adjust), 2*( EdgeControlSize + adjust));
-    }
-
-    QPainterPath shape() const
-    {
-        QPainterPath path;
-        path.addEllipse(-EdgeControlSize, -EdgeControlSize, 2*EdgeControlSize, 2*EdgeControlSize);
-        return path; 
-    }
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
-    {
-        if( fixed())
-        {
-        
-        } else
-        {
-            if (option->state & QStyle::State_Sunken) {
-                painter->setBrush( Qt::gray);
-                painter->setPen( QPen( Qt::black, 0));
-            } else
-            {
-                painter->setBrush( Qt::lightGray);
-                painter->setPen( QPen(Qt::darkGray, 0));
-            }
-            painter->drawEllipse(-EdgeControlSize, -EdgeControlSize,
-                                  2*EdgeControlSize, 2*EdgeControlSize);
-        }
-    }
-    
-    void mousePressEvent(QGraphicsSceneMouseEvent *event)
-    {
-        update();
-        QGraphicsItem::mousePressEvent( event);
-    }
-
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
-    {
-        update();
-        QGraphicsItem::mouseReleaseEvent(event);
-    }
-    
+    QRectF boundingRect() const;
+    QPainterPath shape() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
     void prepareRemove();
-
     void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
     QVariant itemChange(GraphicsItemChange change, const QVariant &value);
 };
@@ -172,198 +117,31 @@ public:
             d->setPred( this);
         } 
     }
-    void adjust()
-    {
-        srcP = mapFromItem( srcControl, 0, 0);
-        dstP = mapFromItem( dstControl, 0, 0);
-        
-        topLeft.setX( min< qreal>( srcP.x(), dstP.x()));
-        topLeft.setY( min< qreal>( srcP.y(), dstP.y()));
-        btmRight.setX( max< qreal>( srcP.x(), dstP.x()));
-        btmRight.setY( max< qreal>( srcP.y(), dstP.y()));
 
-        QLineF mainLine = QLineF( srcP, dstP);
-        qreal size = Abs< qreal>(( min< qreal>( Abs< qreal>( mainLine.dx()),
-                                                Abs< qreal>(mainLine.dy()))));
-        //Stub
-        if( size < 2* EdgeControlSize)
-        {
-            size = 2* EdgeControlSize;
-        }
-        if( size > 20 * EdgeControlSize)
-        {
-            size = 20 * EdgeControlSize;
-        }
+    void adjust();
+    EdgeControl *addControl( QPointF p);
 
-        /** Place cp1 */
-        if ( IsNotNullP( srcControl->pred()))
-        {
-            QPointF p1 = mapFromScene( srcControl->pred()->src()->pos());
-            QLineF line( p1, dstP);
-            QPointF cp1_offset = QPointF( (line.dx() * size)/ line.length(),
-                                          (line.dy() * size)/ line.length());
-            cp1 = srcP + cp1_offset;
-        } else
-        {
-            QPointF cp1_offset = QPointF( (mainLine.dx() * size)/ mainLine.length(),
-                                          (mainLine.dy() * size)/ mainLine.length());
-            cp1 = srcP + cp1_offset;
-        }
-        
-        topLeft.setX( min< qreal>( topLeft.x(), cp1.x()));
-        topLeft.setY( min< qreal>( topLeft.y(), cp1.y()));
-        btmRight.setX( max< qreal>( btmRight.x(), cp1.x()));
-        btmRight.setY( max< qreal>( btmRight.y(), cp1.y()));
-
-        /** Place cp2 */
-        if ( IsNotNullP( dstControl->succ()))
-        {
-            QPointF p2 = mapFromScene( dstControl->succ()->dst()->pos());
-            QLineF line( p2, srcP);
-            QPointF cp2_offset = QPointF( (line.dx() * size)/ line.length(),
-                                          (line.dy() * size)/ line.length());
-            cp2 = dstP + cp2_offset;
-        } else
-        {
-            QPointF cp2_offset = QPointF( -(mainLine.dx() * size)/ mainLine.length(),
-                                          -(mainLine.dy() * size)/ mainLine.length());
-            cp2 = dstP + cp2_offset;
-        }
-        
-        topLeft.setX( min< qreal>( topLeft.x(), cp2.x()));
-        topLeft.setY( min< qreal>( topLeft.y(), cp2.y()));
-        btmRight.setX( max< qreal>( btmRight.x(), cp2.x()));
-        btmRight.setY( max< qreal>( btmRight.y(), cp2.y()));
-
-        prepareGeometryChange();
-    }
-
-    EdgeSegment( EdgeW *e, EdgeControl *src, EdgeControl* dst, QGraphicsScene* scene): 
-        QGraphicsItem( 0, scene),
-        edge( e),
-        srcControl(src),
-        dstControl( dst)
-    {
-        Assert( IsNotNullP( src));
-        Assert( IsNotNullP( dst));
-        //setCacheMode(DeviceCoordinateCache);
-        //setFlag( ItemIsMovable);
-        setZValue(0);
-        src->setSucc( this);
-        dst->setPred( this);
-        adjust();
-    }
-    
-    ~EdgeSegment()
-    {
-        if( IsNotNullP( src()) && src()->succ() == this)
-        {
-            src()->setSucc( NULL);
-        }
-        if( IsNotNullP( dst()) && dst()->pred() == this)
-        {
-            dst()->setPred( NULL);
-        }
-        prepareGeometryChange();
-        update();
-    }
-
-    QRectF boundingRect() const
-    {
-        qreal penWidth = 1;
-        qreal extra = penWidth / 2.0;
-
-        return QRectF( topLeft,
-                       QSizeF( btmRight.x() - topLeft.x(),
-                               btmRight.y() - topLeft.y()))
-               .normalized()
-               .adjusted(-extra, -extra, extra, extra);
-    }
-
-    QPainterPath shape() const
-    {
-        QPainterPath path( srcP);
-        QPainterPathStroker stroker;
-        path.cubicTo( cp1, cp2, dstP);
-        stroker.setWidth( 2);
-        return stroker.createStroke( path); 
-    }
-
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
-    {
-        QPainterPath path( srcP);
-        QPainterPathStroker stroker;
-        path.cubicTo( cp1, cp2, dstP);
-        
-        // Select the pen
-        if( option->state & QStyle::State_Selected)
-        {
-            painter->setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-        } else
-        {
-            painter->setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-        }
-        
-        stroker.setWidth( 4);
-        
-        painter->drawPath(path);
-        
-        /** For illustrative purposes */
-        painter->setPen(QPen(Qt::gray, 1, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin));
-        //painter->drawPath( stroker.createStroke( path));
-        //painter->drawLine( srcP, dstP);
-        //painter->drawLine( srcP, cp1);
-        //painter->drawLine( cp2, dstP);
-    }
-
-    void mousePressEvent(QGraphicsSceneMouseEvent *event)
-    {
-        update();
-        if ( event->button() & Qt::RightButton)
-        {
-            adjust();
-        }
-        if (event->button() == Qt::LeftButton && (flags() & ItemIsSelectable)) {
-            bool multiSelect = (event->modifiers() & Qt::ControlModifier) != 0;
-            if (!multiSelect) {
-                if (!isSelected()) {
-                    if (scene())
-                        scene()->clearSelection();
-                    setSelected(true);
-                }
-            }
-        } else if (!(flags() & ItemIsMovable)) {
-//            event->ignore();
-        }
-    }
-
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
-    {
-        update();
-        if ( event->button() & Qt::RightButton)
-        {
-            adjust();
-        }
-        QGraphicsItem::mouseReleaseEvent(event);
-    }
-    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
-    {
-        update();
-        if ( event->button() & Qt::LeftButton)
-        {
-            EdgeControl *control = new EdgeControl( edge, scene());
-            control->setPos( event->pos());
-            EdgeSegment* seg = new EdgeSegment( edge, control, dst(), scene()); 
-            setDst( control);
-            adjust();
-        }
-        QGraphicsItem::mouseDoubleClickEvent(event);
-    }
+    EdgeSegment( EdgeW *e, EdgeControl *src, EdgeControl* dst, QGraphicsScene* scene);
+    ~EdgeSegment();
+    QRectF boundingRect() const;
+    QPainterPath shape() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
 
 };
 
 class EdgeW: public QGraphicsItem, EdgeT< GraphW, NodeW, EdgeW>
 {
+public:        
+    typedef enum EdgeMode
+    {
+        ModeShow,
+        ModeEdit
+    } EdgeMode;
+
+private:
     qreal arrowSize;
     QPointF sourcePoint;
     QPointF destPoint;
@@ -375,13 +153,9 @@ class EdgeW: public QGraphicsItem, EdgeT< GraphW, NodeW, EdgeW>
     EdgeControl* srcControl;
     EdgeControl* dstControl;
 
-    typedef enum EdgeMode
-    {
-        ModeShow,
-        ModeEdit
-    } EdgeMode;
 
-    EdgeMode mode;
+
+    EdgeMode curr_mode;
 
     /** Constructors are made private, only nodes and graph can create edges */
     EdgeW( GraphW *graph_p, int _id, NodeW *_pred, NodeW* _succ):
@@ -389,7 +163,7 @@ class EdgeW: public QGraphicsItem, EdgeT< GraphW, NodeW, EdgeW>
         {
             srcControl = 0;
             dstControl = 0;
-            mode = ModeShow;
+            curr_mode = ModeShow;
             setFlag( ItemIsSelectable);
         };
         
@@ -415,6 +189,9 @@ public:
 
     void adjust();
     void addControl( EdgeControl* control);
+    void showControls();
+    void hideControls();
+
     inline EdgeControl *srcCtrl() const
     {
         return srcControl;
@@ -423,6 +200,17 @@ public:
     {
         return dstControl;
     }
+
+    inline EdgeMode mode() const
+    {
+        return curr_mode;
+    }
+
+    inline void setMode( EdgeMode m)
+    {
+        curr_mode = m;
+    }
+
     QRectF boundingRect() const;
     QPainterPath shape() const;
     
