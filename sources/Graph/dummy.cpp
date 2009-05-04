54842334a764f9e28176bf48a8dd86b59c52e0ca
Boris Shurygin <boris-shurygin@users.noreply.github.com>
2009-05-04 20:37:39 +0000

    Added dummy file for creation of library graph.lib

---
+++ sources/Graph/dummy.cpp
@@ -0,0 +1,6 @@
+/**
+ * File: dummy.cpp - Graph class definition/implementation, part of
+ * Graph library, internal representation of graphs in ShowGraph tool.
+ * Copyright (C) 2009  Boris Shurygin
+ */
+#include "graph_iface.h"
