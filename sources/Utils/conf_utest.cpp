20b21acd770b7032c4bf54ceee8fd4412d569309
Boris Shurygin <boris-shurygin@users.noreply.github.com>
2009-11-09 16:10:23 +0000

    Options system minimal functionality

--- sources/Utils/conf_utest.cpp
+++ sources/Utils/conf_utest.cpp
@@ -14,16 +14,41 @@
 bool uTestConf()
 {
     Conf *conf = new Conf();
-    conf->addOption( new Option( OPT_STRING, "o", "output", "output file name"));
-    conf->printOpts();
+    
+    /** Create some example options */
+    conf->addOption( new Option( OPT_STRING, "o", "output", "output file name( string option example)"));
+    conf->addOption( new Option( OPT_BOOL, "b", "boolean", "boolean option example"));
+    conf->addOption( new Option( OPT_INT, "i", "integer", "integer option example"));
+    conf->addOption( new Option( OPT_FLOAT, "f", "float", "float option example"));
+    conf->printOpts(); // Print them to console
 
-    char *args[ 5];
+    /** Check that created options can be accessed */
+    assert( isNullP( conf->option( "file")));
+    assert( isNotNullP( conf->option("output")));
+    assert( isNotNullP( conf->shortOption( "o")));
+    assert( isNullP( conf->shortOption( "output")));
+    assert( isNotNullP( conf->longOption("output")));
+
+    /** Create array of arguments */
+    char *args[ 8];
     args[ 0] = "string";
     args[ 1] = "--output";
     args[ 2] = "file";
     args[ 3] = "-a";
     args[ 4] = "--b";
+    args[ 5] = "-b";
+    args[ 6] = "-i";
+    args[ 7] = "80";
 
-    conf->readArgs( 5, args);
+    /** Read arguments from array and parse them */
+    conf->readArgs( 8, args);
+
+    /** Check options values */
+    assert( conf->unknownOptsNum() == 3); // Check number of unknown arguments
+    assert( !(conf->option( "output")->string().compare("file")));
+    Option *int_opt = conf->option( "integer");
+    assert( int_opt->intVal()== 80);
+    assert( conf->option( "b")->isSet());
+    
     return true;
 }
