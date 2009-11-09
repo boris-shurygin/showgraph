2d7fa8b202364545ea119049714937344b12947d
Boris Shurygin <boris-shurygin@users.noreply.github.com>
2009-11-09 16:48:10 +0000

    Some convenience macros for traversing nodes/edges

--- sources/Graph/graph_iface.h
+++ sources/Graph/graph_iface.h
@@ -16,7 +16,7 @@
 #include "agraph.h"
 
 /**
- * Convinience macro for traversing edges
+ * Convenience macro for traversing edges in given direction
  * parameter: node - a node that we use to get first edge
  * parameter: edge - an object of Edge type or subclass of Edge which is the loop variable
  * parameter: dir - 'Succ' or 'Pred'
@@ -24,4 +24,58 @@
 #define ForEdges(node, edge, dir) for ( edge = node->first##dir();\
 										isNotNullP( edge);\
 								        edge = edge->next##dir())
+/** Succs traverse implementation */
+#define ITERATE_NODE_SUCCS(edge, node) for ( edge = node->firstSucc();\
+										     isNotNullP( edge);\
+								             edge = edge->nextSucc())
+
+/** Preds traverse implementation */
+#define ITERATE_NODE_PREDS(edge, node) for ( edge = node->firstPred();\
+									         isNotNullP( edge);\
+								             edge = edge->nextPred())
+/**
+ * Convenience macro for traversing successors of node
+ * parameter: edge - an object of Edge type or subclass of Edge which is the loop variable
+ * parameter: node - a node that we use to get first edge
+ */
+#ifndef foreachSucc
+#  define foreachSucc(edge, node) ITERATE_NODE_SUCCS(edge, node)
+#endif
+
+/**
+ * Convenience macro for traversing predecessors of node
+ * parameter: edge - an object of Edge type or subclass of Edge which is the loop variable
+ * parameter: node - a node that we use to get first edge
+ */
+#ifndef foreachPred
+#  define foreachPred(edge, node) ITERATE_NODE_PREDS(edge, node)
+#endif
+
+/** Graph's edges traverse implementation */
+#define ITERATE_GRAPH_EDGES(edge, graph) for ( edge = graph->firstEdge();\
+									           isNotNullP( edge);\
+								               edge = edge->nextEdge())
+/** Graph's nodes traverse implementation */
+#define ITERATE_GRAPH_NODES(node, graph) for ( node = graph->firstNode();\
+									           isNotNullP( node);\
+								               node = node->nextNode())
+
+/**
+ * Convenience macro for traversing edges in graph
+ * parameter: edge - an object of Edge type or subclass of Edge which is the loop variable
+ * parameter: graph - graph of interest
+ */
+#ifndef foreachEdge
+#  define foreachEdge(edge, graph) ITERATE_GRAPH_EDGES(edge, graph)
+#endif
+
+/**
+ * Convenience macro for traversing nodes in graph
+ * parameter: node - an object of Node type or subclass of Node which is the loop variable
+ * parameter: graph - graph of interest
+ */
+#ifndef foreachNode
+#  define foreachNode(node, graph) ITERATE_GRAPH_NODES(node, graph)
+#endif
+
 #endif
