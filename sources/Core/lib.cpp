4fd9479b779e32a3b363a7b6a408a2349be2cea8
Boris Shurygin <boris-shurygin@users.noreply.github.com>
2011-07-17 10:34:49 +0000

    added missing file

---
+++ sources/Core/lib.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file: lib.cpp
+ * Dummy file for library generation
+ */
+/*
+ * Graph library, internal representation of graphs in ShowGraph tool.
+ * Copyright (C) 2009  Boris Shurygin
+ */
+#include "core_iface.h"
